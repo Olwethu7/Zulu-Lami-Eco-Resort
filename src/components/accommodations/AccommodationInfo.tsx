738bd6371e301d9ca474f4773e439a08f5b81a72
Olwethu7 <132436323+Olwethu7@users.noreply.github.com>
2025-11-12 16:28:57 +0200

    Update AccommodationInfo.tsx

--- src/components/accommodations/AccommodationInfo.tsx
+++ src/components/accommodations/AccommodationInfo.tsx
@@ -1,4 +1,5 @@
 import { Badge } from "@/components/ui/badge";
+
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import {
