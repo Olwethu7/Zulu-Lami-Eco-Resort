29c9a9b5e582aa7a54943e1eb44f87531cbda5e3
Olwethu7 <132436323+Olwethu7@users.noreply.github.com>
2025-11-12 16:36:31 +0200

    Update ProfileForm.tsx

--- src/components/profile/ProfileForm.tsx
+++ src/components/profile/ProfileForm.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+
 import { useAuth } from "@/hooks/useAuth";
 import { useNavigate } from "react-router-dom";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
